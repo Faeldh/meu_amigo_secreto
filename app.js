1e942f2198402570bee12a4c9cbec5b736529a0e
Rafael Almeida <rafaeldealmeida138@gmail.com>
2025-08-20 19:36:46 -0300

    01

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Contante que pega o botao adiconarAmigos e fixa na variavel adcionar amigos
+const adcionarAmigo = document.querySelector('#adcionarAmigo');
+//Varial que receberar os nomes dos amigos(Do Tipo array)
+let amigos = [];
+//Fução que vai receber o nomes
+function adcionarAmigo(){
+   //Variavel que recebe os nomes 
+    let input = document.querySelector('#nomeAmigos');
+    let nome = input.value.trim();
+    amigos.push(nome);
+    console.log(amigos)
+}
+
+
+//def nomeAmigod()
