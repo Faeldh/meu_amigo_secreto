068570c7775fd2c7b8d6dd40465a1a637f83191d
Rafael Almeida <rafaeldealmeida138@gmail.com>
2025-08-20 21:20:11 -0300

    01

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ function adcionarAmigo(){
    //Variavel que recebe os nomes 
     let input = document.querySelector('#nomeAmigos');
     let nome = input.value.trim();
-    amigos.push(nome);
-    console.log(amigos);
-    alert("Adicionado com sucesso!");
+    if(nome){
+        //Adiciona o nome no array amigos
+       amigos.push(nome); 
+       //Limpa o input
+       input.value = '';
+       console.log(amigos);
+       alert("Adcionado com sucesso!");
+    }else{
+        alert("Por favor, digite um nome válido.");
+    }
+    
+    
 }
-
